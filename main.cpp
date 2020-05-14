762785b96633c7a42cfbe019cf9fbbc09014287c
Giovane Simamoto <60979790+GiSima@users.noreply.github.com>
2020-05-14 08:39:58 -0300

    Leitura da escolaridade

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int escola;
+
+    cout << "Qual sua escolaridade?" << endl;
+    cout << "1) Ensino Fundamental" << endl;
+    cout << "2) Ensino Medio" << endl;
+    cout << "3) Ensino Superior" << endl;
+    cout << "4) Pos-graduacao" << endl;
+    cout << "Digite uma opcao: ";
+    cin >> escola; cout << endl;
 }
