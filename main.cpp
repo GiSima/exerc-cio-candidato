32c23563b289b2c5c95a6788b1dfaa2feb16d4c8
Giovane Simamoto <60979790+GiSima@users.noreply.github.com>
2020-05-14 08:45:52 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-    int escola;
+    int escola, job;
+    char viajem, hab;
 
     cout << "Qual sua escolaridade?" << endl;
     cout << "1) Ensino Fundamental" << endl;
@@ -13,4 +14,10 @@ int main()
     cout << "4) Pos-graduacao" << endl;
     cout << "Digite uma opcao: ";
     cin >> escola; cout << endl;
+
+    cout << "Voce tem quantos anos de experiencia profissional? "; cin >> job;
+    cout << "Voce tem disponibilidade para viajar (S/N)? "; cin >> viajem;
+    cout << "Voce tem habilitacao de motorista (S/N)? "; cin >> hab;
+
+    return 0;
 }
